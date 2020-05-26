2f475d5cf44b010b15bb84729675a6ef9e87c650
unknown <candales.roberto@gmail.com>
2020-05-26 11:03:05 -0300

    Updates

---
+++ precache-manifest.568573d2ec0893b6ff305bc76a8b6448.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f24845f7a950454194d0abc19fb953",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "993523b90341f38ca087",
+    "url": "/React/static/css/main.23969380.chunk.css"
+  },
+  {
+    "revision": "c6aa53ce7d0803866209",
+    "url": "/React/static/js/2.e237e1cf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React/static/js/2.e237e1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993523b90341f38ca087",
+    "url": "/React/static/js/main.f7d6935c.chunk.js"
+  },
+  {
+    "revision": "f1a27b2f0b204a64982d",
+    "url": "/React/static/js/runtime-main.30dbeb8e.js"
+  },
+  {
+    "revision": "494ead52cedcf68093c79e9275756acf",
+    "url": "/React/static/media/002.494ead52.jpg"
+  },
+  {
+    "revision": "75823db4c8c8451bf56fd2d427502dae",
+    "url": "/React/static/media/MusicApp2.75823db4.mp4"
+  },
+  {
+    "revision": "96b2fcd50b6e9b81db8832a9a7643ab7",
+    "url": "/React/static/media/RC.96b2fcd5.jpg"
+  },
+  {
+    "revision": "0db5977bf7abf04a3be42f9c99906641",
+    "url": "/React/static/media/home.0db5977b.png"
+  },
+  {
+    "revision": "635322f36b3efaa2128ad983350893ad",
+    "url": "/React/static/media/native.635322f3.png"
+  },
+  {
+    "revision": "a1fbc01444b302e8c95c96a0b7ee7730",
+    "url": "/React/static/media/showVideo.a1fbc014.mp4"
+  },
+  {
+    "revision": "8c53b9cfe874eb558aa25288f94ebafe",
+    "url": "/React/static/media/stay.8c53b9cf.jpg"
+  }
+]);
